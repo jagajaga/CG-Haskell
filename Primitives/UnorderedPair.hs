bcc3a01d5e805a90280e0e3998fa50ea81828363
jagajaga <senia00@gmail.com>
2013-11-25 20:07:15 +0400

    add unordered pair

---
+++ Primitives/UnorderedPair.hs
@@ -0,0 +1,23 @@
+module Primitives.UnorderedPair where
+
+import Data.Hashable
+
+data UnorderedPair a = UnorderedPair !a !a deriving (Show, Eq)
+
+makeUnorderedPair :: (Ord a) => a -> a -> UnorderedPair a
+makeUnorderedPair a b 
+    | a <= b = UnorderedPair a b 
+    | otherwise = UnorderedPair b a
+
+instance (Hashable a, Ord a) => Hashable (UnorderedPair a) where
+  hashWithSalt s (UnorderedPair a b) = hashWithSalt s (a, b)
+
+other :: (Eq a) => a -> UnorderedPair a -> Maybe a
+other x (UnorderedPair a b) 
+    | x == a  = Just b 
+    | x == b = Just a
+    | otherwise =  Nothing
+
+isElem :: (Eq a) => a -> UnorderedPair a -> Bool
+isElem x p = case other x p of
+  Just _ -> True ; _ -> False
