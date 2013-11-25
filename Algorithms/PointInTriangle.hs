bbd58571aa7c193b8541feaf53fe2b1824ac5d6e
jagajaga <senia00@gmail.com>
2013-11-25 23:07:08 +0400

    add point localisation in triangle

---
+++ Algorithms/PointInTriangle.hs
@@ -0,0 +1,12 @@
+module Algorithms.PointInTriangle where
+
+import           Algorithms.Rotate
+import           Graphics.Gloss.Data.Point
+import           Primitives.Triangle
+
+pointInTriangle :: Point -> Triangle -> Bool
+pointInTriangle p (p1, p2, p3) = not or1 && not or2 && not or3
+    where
+        or1 = clockwise p p1 p2
+        or2 = clockwise p p2 p3
+        or3 = clockwise p p3 p1
