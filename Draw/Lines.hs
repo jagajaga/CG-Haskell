de158f460123fe8d808514f1b1875247ea86f58f
jagajaga <senia00@gmail.com>
2013-11-27 00:30:44 +0400

    refacroder

--- Draw/Lines.hs
+++ Draw/Lines.hs
@@ -1,21 +1,14 @@
-module Draw.Lines (drawPath, drawClosedLines, drawPathFromState, drawClosedLinesFromState) where
+module Draw.Lines (drawPath, drawClosedLines) where
 
 import           Control.Lens
 import           Graphics.Gloss
-import           State.State
 
 myHead :: [a] -> [a]
 myHead [] = []
 myHead (x:_) = [x]
 
-drawPathFromState :: State -> Picture
-drawPathFromState = drawPath . flip (^.) points
-
 drawPath :: [Vector] -> Picture
 drawPath = Color white . Line
-
-drawClosedLinesFromState :: State -> Picture
-drawClosedLinesFromState = drawClosedLines . flip (^.) points
 
 drawClosedLines :: [Vector] -> Picture
 drawClosedLines s = Color white . Line $ ss
