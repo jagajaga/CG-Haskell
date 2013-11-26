662b111b4cfb65f3dff842b2a96f139bacfdd8ad
jagajaga <senia00@gmail.com>
2013-11-27 00:30:37 +0400

    refactored

--- Draw/Points.hs
+++ Draw/Points.hs
@@ -1,11 +1,7 @@
-module Draw.Points (drawPoints, drawPointsFromState) where
+module Draw.Points (drawPoints) where
 
 import           Control.Lens
 import           Graphics.Gloss
-import           State.State
-
-drawPointsFromState :: State -> Picture
-drawPointsFromState = drawPoints . flip (^.) points
 
 drawPoints :: [Vector] -> Picture
 drawPoints = Pictures . map (\(x,y) -> Color green $ Translate x y $ Circle 3)
