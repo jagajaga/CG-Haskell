296075cb9606c418d307622f218df0e85210621c
jagajaga <senia00@gmail.com>
2013-11-28 00:32:09 +0400

    add point in box algo

--- Primitives/BoundBox.hs
+++ Primitives/BoundBox.hs
@@ -1,8 +1,8 @@
-module Primitives.BoundBox(BoundBox (..), boundPoints) where
+module Primitives.BoundBox(BoundBox (..), boundPoints, pointInBoundBox) where
 
 import           Graphics.Gloss.Data.Point
 
-data BoundBox a = BoundBox !a !a !a !a
+data BoundBox a = BoundBox !a !a !a !a deriving (Show, Eq)
 
 boundPoints :: Ord a => [(a, a)] -> BoundBox a
 boundPoints ps =
@@ -10,3 +10,6 @@ boundPoints ps =
     xs = map fst ps
     ys = map snd ps
   in BoundBox (minimum xs) (minimum ys) (maximum xs) (maximum ys)
+
+pointInBoundBox :: Point -> BoundBox (Float) -> Bool
+pointInBoundBox pt (BoundBox a b c d) = pointInBox pt (a, b) (c, d)
