a515e1a5991fbc4fffd6e822df81fe11c6ffee94
jagajaga <senia00@gmail.com>
2013-11-28 22:05:01 +0400

    refactor

--- Draw/ConvexHullAndrew.hs
+++ Draw/ConvexHullAndrew.hs
@@ -3,11 +3,12 @@ module Draw.ConvexHullAndrew(drawConvexHullAndrew) where
 import           Algorithms.ConvexHull.Andrew
 import           Control.Lens
 import           Draw.Lines
-import           Draw.Points(drawPointsFromState)
+import           Draw.Points
 import           Graphics.Gloss
 import           State.State
 
 drawConvexHullAndrew :: State -> Picture
-drawConvexHullAndrew state = Pictures (drawPointsFromState state : [drawClosedLinesFromState (state & (points .~ convexHullPoints))])
+drawConvexHullAndrew state = Pictures (drawPoints pts : [drawClosedLines (convexHullPoints)])
     where
+        pts = state ^.points
         convexHullPoints = convexHullAndrew $ state^.points
